47fe145d20d867acd37731d2de83a6908950e9a4
Mihalovici Tudor <105587619+v4n00@users.noreply.github.com>
2022-11-30 12:59:13 +0200

    Implemented a basic Event class

--- src/cpp/event.cpp
+++ src/cpp/event.cpp
@@ -1,1 +1,96 @@
 #include "../Headers/Event.h"
+
+// - Setters
+
+void Event::setName(const char* name) {
+	if (strlen(name) > 1)
+		this->name = Util::deepCopy(name);
+	else throw;
+}
+
+void Event::setStartTime(Date* startTime) {
+	if (startTime != nullptr)
+		this->startTime = new Date(*startTime);
+	else throw;
+}
+
+void Event::setRuntime(unsigned int runtime) {
+	if (runtime > 1)
+		this->runtime = runtime;
+	else throw;
+}
+
+void Event::setRoom(Room* room) {
+	// this is intended to be a shallow copy, an event can change the room it's in
+	// and most likely 1 room is not going to change at all
+	this->room = room;
+}
+
+// - Getters
+
+char* Event::getName() {
+	return Util::deepCopy(this->name);
+}
+
+Date* Event::getStartTime() {
+	Date* rez = new Date(*this->startTime);
+	return rez;
+}
+
+unsigned int Event::getRuntime() {
+	return this->runtime;
+}
+
+Room* Event::getRoom() {
+	Room* rez = new Room(*this->room);
+	return rez;
+}
+
+// - Constructors / Destructor
+
+Event::Event(const Event& anotherEvent) {
+	this->setName(anotherEvent.name);
+	this->setStartTime(anotherEvent.startTime);
+	this->setRuntime(anotherEvent.runtime);
+	this->setRoom(anotherEvent.room);
+}
+
+Event::Event(const char* name, Date& startTime, unsigned int runtime, Room& room) {
+	this->setName(name);
+	this->setStartTime(&startTime);
+	this->setRuntime(runtime);
+	this->setRoom(&room);
+}
+
+Event::Event() {
+
+}
+
+Event::~Event() {
+	delete startTime;
+	this->startTime = nullptr;
+	delete[] name;
+	this->name = nullptr;
+	// again the Room stays constant, so no need to delete the Room object
+	this->room = nullptr;
+}
+
+// - Operators
+
+// copy assignment
+
+Event Event::operator=(const Event& anotherRoom) {
+	this->setName(name);
+	this->setStartTime(startTime);
+	this->setRuntime(runtime);
+	this->setRoom(room);
+	return *this;
+}
+
+// string typecast
+
+Event::operator std::string() {
+	std::stringstream ss;
+	ss << "Event \"" << name << "\" is taking place on " << (std::string)*startTime << " with a runtime of " << runtime << " minutes and takes place in " << (std::string)*room;
+	return ss.str();
+}
--- src/headers/event.h
+++ src/headers/event.h
@@ -2,17 +2,59 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <ctime>
-#include "Room.h"
-#include "Seat.h"
+#include "room.h"
+#include "date.h"
 
 class Event {
 private:
-	std::string name = "";
-	/*Date date;*/
+	char* name = nullptr;
+	Date* startTime = nullptr;
 	unsigned int runtime = 0;
-	Room* room;
+	Room* room = nullptr;
+
+	// - Setters
+
+	void setName(const char* name);
+	void setStartTime(Date* startTime);
+	void setRuntime(unsigned int runtime);
+	void setRoom(Room* room);
 
 public:
+	// - Public interface
+
+	// - Getters
+	char* getName();
+	Date* getStartTime();
+	unsigned int getRuntime();
+	Room* getRoom();
+
+	// - Constructor / Destructor
+
+	Event(const Event& anotherEvent);
+	// make an event from already existing data
+	Event(const char* name, Date& startTime, unsigned int runtime, Room& room);
+	// make an empty event
+	Event();
+	~Event();
+
+	// - Operators
+
+		// copy assignment
+
+	Event operator=(const Event& anotherRoom);
+
+		// typecast operator
+
+	explicit operator std::string();
+
+		// compare operators
+
+	bool operator==(const Event& anotherRoom);
+	bool operator!=(const Event& anotherRoom);
+
+		// stream operators
+
+	friend std::ostream& operator << (std::ostream& out, const Event& room);
+	friend std::istream& operator >> (std::istream& in, Event& room);
 
 };
