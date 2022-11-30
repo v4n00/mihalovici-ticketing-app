246b5dea56217eec99d1300992e7a0f6583852e1
Mihalovici Tudor <105587619+v4n00@users.noreply.github.com>
2022-11-30 18:18:41 +0200

    Implemented basic Location class

--- src/cpp/location.cpp
+++ src/cpp/location.cpp
@@ -1,2 +1,174 @@
 #include "../headers/location.h"
 
+unsigned int Location::TOTAL_LOCATIONS = 0;
+
+// - Setters
+
+void Location::setLocationId(unsigned int locationId) {
+	if (locationId != 0)
+		this->locationId = locationId;
+	else throw;
+}
+
+void Location::setName(const char* name) {
+	if (strlen(name) > 1)
+		this->name = Util::deepCopy(name);
+	else throw;
+}
+
+void Location::setAddress(const char* address) {
+	if (strlen(address) > 1)
+		this->address = Util::deepCopy(address);
+	else throw;
+}
+
+void Location::setEvents(Event** events) {
+	if (events != nullptr) {
+		this->events = new Event * [runningEvents];
+		for (size_t i = 0; i < runningEvents; ++i)
+			this->events[i] = new Event(*(events[i]));
+	}
+	else throw;
+}
+
+void Location::setRunningEvents(unsigned int runningEvents) {
+	if (runningEvents != 0)
+		this->runningEvents = runningEvents;
+	else throw;
+}
+
+// - Getters
+
+char* Location::getName() {
+	return Util::deepCopy(this->name);
+}
+
+char* Location::getAddress() {
+	return Util::deepCopy(this->address);
+}
+
+Event** Location::getEvents() {
+	Event** rez = new Event * [runningEvents];
+	for (size_t i = 0; i < runningEvents; ++i)
+		rez[i] = new Event(*(this->events[i]));
+	return rez;
+}
+
+unsigned int Location::getRunningEvents() {
+	return this->runningEvents;
+}
+
+// - Constructors / Destructor
+
+
+
+Location::Location(const Location& anotherLocation) {
+	this->setLocationId(anotherLocation.locationId);
+	this->setName(anotherLocation.name);
+	this->setAddress(anotherLocation.address);
+	this->setRunningEvents(anotherLocation.runningEvents);
+	this->setEvents(anotherLocation.events);
+	++TOTAL_LOCATIONS;
+}
+
+Location::Location(const char* name, const char* address, Event** events, unsigned int runningEvents) {
+	this->setLocationId(++TOTAL_LOCATIONS);
+	this->setName(name);
+	this->setAddress(address);
+	this->setRunningEvents(runningEvents);
+	this->setEvents(events);
+}
+
+Location::Location() {
+	this->setLocationId(++TOTAL_LOCATIONS);
+}
+
+Location::~Location() {
+	delete[] name;
+	this->name = nullptr;
+	delete[] address;
+	this->address = nullptr;
+	for (size_t i = 0; i < runningEvents; ++i)
+		delete events[i];
+	delete[] events;
+	--TOTAL_LOCATIONS;
+}
+
+// - Operators
+
+// copy assignment
+
+Location Location::operator=(const Location& anotherLocation) {
+	this->setLocationId(anotherLocation.locationId);
+	this->setName(anotherLocation.name);
+	this->setAddress(anotherLocation.address);
+	this->setRunningEvents(anotherLocation.runningEvents);
+	this->setEvents(anotherLocation.events);
+	return *this;
+}
+
+// string typecast
+
+Location::operator std::string() {
+	std::stringstream ss;
+	ss << "Location #" << locationId << " named \"" << name << "\" at address " << address << " has " << runningEvents << " running event(s)";
+	return ss.str();
+}
+
+// compare operators
+
+bool Location::operator==(const Location& anotherLocation) {
+	if (this == &anotherLocation)
+		return true;
+	if (this->locationId != anotherLocation.locationId)
+		return false;
+	if (strcmp(this->name, anotherLocation.name) != 0)
+		return false;
+	if (strcmp(this->address, anotherLocation.address) != 0)
+		return false;
+	if (this->events != anotherLocation.events)
+		return false;
+	if (this->runningEvents = anotherLocation.runningEvents)
+		return false;
+	return true;
+}
+
+bool Location::operator!=(const Location& anotherLocation) {
+	return !(*this == anotherLocation);
+}
+
+// stream operators
+
+std::ostream& operator << (std::ostream& out, const Location& location) {
+	out << "L" << location.locationId << "-" << location.name << "-" << location.address << "-" << location.runningEvents << ":";
+	for (size_t i = 0; i < location.runningEvents; ++i)
+		out << *(location.events[i]);
+	out << ";";
+	return out;
+}
+
+std::istream& operator >> (std::istream& in, Location& location) {
+	std::string locationId;
+	std::string name;
+	std::string address;
+	std::string runningEvents;
+	if (std::getline(in, locationId, 'L') &&
+		std::getline(in, locationId, '-') &&
+		std::getline(in, name, '-') &&
+		std::getline(in, address, '-') &&
+		std::getline(in, runningEvents, ':')) {
+		location.setLocationId(std::stoi(locationId));
+		location.setName(name.c_str());
+		location.setAddress(address.c_str());
+		location.setRunningEvents(std::stoi(runningEvents));
+	}
+	else throw;
+	location.events = new Event * [location.runningEvents];
+	for (size_t i = 0; i < location.runningEvents; ++i) {
+		location.events[i] = new Event();
+		if (!(in >> *(location.events[i])))
+			throw;
+	}
+	getline(in, locationId, ';');
+	return in;
+}
--- src/headers/location.h
+++ src/headers/location.h
@@ -10,15 +10,17 @@ private:
 	unsigned int locationId;
 	char* name = nullptr;
 	char* address = nullptr;
-	Event* events = nullptr;
+	Event** events = nullptr;
 	unsigned int runningEvents = 0;
+
+	static unsigned int TOTAL_LOCATIONS;
 
 	// - Setters
 
-	void setLocatioNId(unsigned int locationId);
+	void setLocationId(unsigned int locationId);
 	void setName(const char* name);
 	void setAddress(const char* address);
-	void setEvents(Event* events);
+	void setEvents(Event** events);
 	void setRunningEvents(unsigned int runningEvents);
 
 public:
@@ -29,7 +31,34 @@ public:
 	unsigned int getLocationId();
 	char* getName();
 	char* getAddress();
-	Event* getEvents();
+	Event** getEvents();
 	unsigned int getRunningEvents();
 
+	// - Constructors / Destructor
+
+	// copy constructor
+	Location(const Location& anotherLocation);
+	Location(const char* name, const char* address, Event** events, unsigned int runningEvents);
+	Location();
+	~Location();
+
+	// - Operators
+	
+		// copy assignment
+
+	Location operator=(const Location& anotherLocation);
+
+		// typecast operator
+
+	explicit operator std::string();
+
+		// compare operators
+
+	bool operator==(const Location& anotherLocation);
+	bool operator!=(const Location& anotherLocation);
+
+		// stream operators
+
+	friend std::ostream& operator << (std::ostream& out, const Location& location);
+	friend std::istream& operator >> (std::istream& in, Location& location);
 };
