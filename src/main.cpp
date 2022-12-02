16c044131b6484891b4ad643039ea37e3822cf46
Mihalovici Tudor <105587619+v4n00@users.noreply.github.com>
2022-12-02 14:44:57 +0200

    Last changes before end of Phase #1

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
-#include <string>
-#include "headers/location.h"
+#include "headers/ticket.h"
 
 using namespace std;
 
-//TODO:
-// today++: implement ticket class
-// (today++)++: implement menu class
-
 int main() {
 
-	Room Projector1("Projector1", 1, 38, 4);
-	Room Projector2("Projector2", 1, 48, 7);
+	Room Projector1("Projector1", 1, 40, 4);
+	Room Projector2("Projector2", 1, 46, 7);
 	Location cinema("Cinema", "Str. dov 2");
 	cinema.addEvent("Violet Evergarden", 87, Projector1, 30, 16, 27, Month::DECEMBER, 2022);
 	cinema.addEvent("Death Note", 85, Projector2, 30, 16, 27, Month::DECEMBER, 2022);
-	cinema.getEvents()[0]->getRoom()->printLayout();
-	cinema.getEvents()[1]->getRoom()->printLayout();
-	cout << (string)cinema;
+
+	// display projector1
+
+	Projector1.changeSeatAvailability(3, 4, SeatAvailability::PAID);
+	Projector1.changeSeatAvailability(2, 1, SeatAvailability::RESERVED);
+	Projector1.changeSeatAvailability(4, 7, SeatAvailability::PAID);
+
+	cout << (string)Projector1 << endl;
+	Projector1.printLayout(cout);
+
+	// display projector2
+
+	Projector2.changeSeatAvailability(3, 4, SeatAvailability::PAID);
+	Projector2.changeSeatAvailability(2, 1, SeatAvailability::PAID);
+	Projector2.changeSeatAvailability(3, 3, SeatAvailability::RESERVED);
+
+	cout << (string)Projector2 << endl;
+	Projector2.printLayout(cout);
+
+	// display a location
+
+	cout << endl << (string)cinema << endl;
+	cout << "How it will be stored in a file: " << endl << cinema;
 
 	return 0;
-
 }
