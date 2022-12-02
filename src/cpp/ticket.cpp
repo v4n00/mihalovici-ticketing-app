4aa0ed1adb1e348fd8e22605066498e27d92ffd8
Mihalovici Tudor <105587619+v4n00@users.noreply.github.com>
2022-12-02 14:41:56 +0200

    Added generateId method for Ticket

--- src/cpp/ticket.cpp
+++ src/cpp/ticket.cpp
@@ -1,1 +1,12 @@
-#include "../Headers/Ticket.h"
+#include "../headers/ticket.h"
+
+char* Ticket::generateId() {
+	char* id = new char[9];
+	char hex[16] = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f' };
+	srand(time(NULL));
+	for (int i = 0; i < 8; ++i) {
+		id[i] = hex[rand() % 16];
+	}
+	id[8] = '\0';
+	return id;
+}
--- src/headers/ticket.h
+++ src/headers/ticket.h
@@ -1,1 +1,34 @@
 #pragma once
+#include <stdlib.h>
+#include <time.h>
+#include "location.h"
+
+// I cannot make this class without an STL class so I'm gonna stop here
+
+class Ticket {
+public:
+	char* ticketId;
+	Event* event;
+
+	static unsigned int* VALID_TICKETS;
+	static unsigned int NO_VALID_TICKETS;
+
+	// - Setter
+	void setEvent(Event* event);
+	void setTicketId(unsigned int ticketId);
+
+	// - Private interface
+
+	static char* generateId();
+
+public:
+
+	// - Getter
+	unsigned int getTicketId();
+	Event* getEvent();
+
+	// - Constructors / Destructor
+	Ticket();
+	~Ticket();
+
+};
